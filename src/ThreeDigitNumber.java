7b23e2e5bd08717cec78572cf574f019fdff0c0c
arinashishkova <112506666+arinashishkova@users.noreply.github.com>
2024-09-30 10:49:43 +0300

    Update ThreeDigitNumber.java

--- src/ThreeDigitNumber.java
+++ src/ThreeDigitNumber.java
@@ -1,22 +1,3 @@
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        // Создаем объект Scanner для ввода данных
-//        Scanner scanner = new Scanner(System.in);
-//
-//        // Вводим значение температуры в градусах Цельсия
-//        System.out.print("Введите температуру в градусах Цельсия: ");
-//        double celsius = scanner.nextDouble();
-//
-//        // Вычисляем температуру в градусах Фаренгейта
-//        double fahrenheit = (celsius * 9 / 5) + 32;
-//
-//        // Выводим результат
-//        System.out.println("Температура по Фаренгейту: " + fahrenheit);
-//
-//    }
-//}
 
 import java.util.Scanner;
 
